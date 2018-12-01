e0c98b845bc323d4060f2cae9e011ec3563d49ed
Matheus Picioli <matheus.picioli98@gmail.com>
2018-12-01 19:34:18 -0200

    Adicionar arquivos de projeto.

---
+++ PrimeiraClasse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiraClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
